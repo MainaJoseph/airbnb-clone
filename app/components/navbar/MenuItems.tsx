22402a68b459e90b33539ac1c1bdce5bf565eeab
MainaJoseph <mainajm254@gmail.com>
2024-01-17 15:13:44 +0300

    Added MenuItems

---
+++ app/components/navbar/MenuItems.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface MenuItemsProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItems: React.FC<MenuItemsProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItems;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,15 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItems from "./MenuItems";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toogleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -14,7 +21,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toogleOpen}
           className="
         py-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
@@ -24,6 +31,16 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItems onClick={() => {}} label="Login" />
+              <MenuItems onClick={() => {}} label="SignUp" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
