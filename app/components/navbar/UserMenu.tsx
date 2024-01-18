4549606024d2e8f3ad8c8e7fd7b280319a8f7d78
MainaJoseph <mainajm254@gmail.com>
2024-01-18 19:06:19 +0300

    Added types Safeuser

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,11 +6,11 @@ import { useCallback, useState } from "react";
 import MenuItems from "./MenuItems";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
 interface UserMenuProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
