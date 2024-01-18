2ed74c30054293ab385adb52c2c7a0443075c0ed
MainaJoseph <mainajm254@gmail.com>
2024-01-18 15:06:45 +0300

    Added a Input  component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -58,7 +58,7 @@ const RegisterModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" center />
       <Input
         id="email"
         label="Email"
