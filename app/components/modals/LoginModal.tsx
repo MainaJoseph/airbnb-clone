ecfbdd19940bbce3578b67e186192f6bd6c86a93
MainaJoseph <mainajm254@gmail.com>
2024-01-18 18:06:24 +0300

    Added actions getCurrentUser.ts

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -61,7 +61,7 @@ const LoginModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome back" subtitle="Login to your account!" />
+      <Heading title="Welcome back" subtitle="Login to your account!" center />
       <Input
         id="email"
         label="Email"
