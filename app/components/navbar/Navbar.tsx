2713a0c10f795d3e7a82b2ddcb93f0a5337d75c0
MainaJoseph <mainajm254@gmail.com>
2024-01-19 14:36:26 +0300

    Added Categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-"use client";
+import { SafeUser } from "@/app/types";
 
+import Categories from "./Categories";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
-import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -13,15 +13,30 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
         <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <div
+            className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+          >
             <Logo />
             <Search />
             <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
