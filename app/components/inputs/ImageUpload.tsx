300e0fd5ccb1fbb9a978cee937d7d2fdcf565dbe
MainaJoseph <mainajm254@gmail.com>
2024-01-29 22:58:24 +0300

    Finished image upload

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,7 +9,7 @@ declare global {
   var cloudinary: any;
 }
 
-const uploadPreset = "pgc9ehd5";
+const uploadPreset = "bbrqblfy";
 
 interface ImageUploadProps {
   onChange: (value: string) => void;
