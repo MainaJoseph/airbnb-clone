0a0b6ba12d44a959d4e9846164ddd65ec7b79f07
MainaJoseph <mainajm254@gmail.com>
2024-01-30 00:16:07 +0300

    Added a listing card to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
-import Image from "next/image";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">AirBnB</div>;
+import getListings, { IListingsParams } from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface HomeProps {
+  searchParams: IListingsParams;
 }
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div
+        className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
